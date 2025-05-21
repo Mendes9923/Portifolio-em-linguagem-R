b9661bcb7fcd77ce8f1ce96cdc8286f9702b2fdf
Mendes9923 <110001026+Mendes9923@users.noreply.github.com>
2025-05-20 22:06:55 -0300

    Add files via upload

---
+++ Portifólio R/Linguagem R/operadores_matematicos.R
@@ -0,0 +1,91 @@
+##################################
+###   OPERADORES MATEM?TICOS   ###
+##################################
+
+# Soma   (linha de coment?rio)
+5 + 3
+
+# Subtra??o
+8 - 6
+
+# Multiplica??o
+4 * 3
+
+# Divis?o
+20 / 5
+
+# Potencia??o
+4 ** 2
+
+# ou
+
+4 ^ 2
+
+# Radicia??o
+
+# raiz quadrada
+sqrt (81)
+
+81**(1/2)
+
+# raiz c?bica
+125**(1/3)
+27**(1/3)
+
+# M?dulo
+abs (-10)
+abs (10)
+
+# Fatorial
+
+factorial(3)   # 3! = 3.2.1 = 6
+factorial(5)   # 5! = 5.4.3.2.1 = 120
+factorial(17)  # 17!
+
+# Logaritmo (base 2 e base 10)
+log10(100)    # 10^2 = 100
+
+log2(8)       # 2^3 = 8
+
+log10(1000)
+
+# Exponencial
+exp(1)       # e = 2,718281828...
+exp(2)
+exp(-2)
+
+# Logaritmo natural
+log(2.718281828)
+log(100)
+
+# Constante pi
+pi
+
+# pi radianos = 180 graus
+
+# Fun??es trigonom?tricas (radianos)
+sin(pi/6)  # pi/6 radianos ? igual a 30 graus
+asin(0.5)
+
+cos(pi/4)  # pi/4 radianos ? igual a 45 graus
+acos(0.7071068)
+
+tan(pi/4)
+atan(1)
+
+# Nota??o cient?fica
+
+2e32 # (2x10^32)
+
+2e3 # 2x10^3
+
+2e-26
+
+2e5 + 4e5
+
+3e20 * 2e12
+
+15e16 / 5e6
+
+#Versão do R
+getRversion()
